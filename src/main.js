95678a8f2f1b337960e5b5241e0b08d27a3424e7
Camellia Shea <cashmill@ucsc.edu>
2020-04-22 12:12:27 -0700

    Set up basic file and folder structure, skeleton of main.js

---
+++ src/main.js
@@ -0,0 +1,13 @@
+/*
+ * WIP Name: Shelving Crisis
+ * 
+ * Single player infinite-climber
+ * by Camellia Shea, Sean Noelle,  and Josepheng Xiong
+ * 
+ * */
+
+
+let config = {
+}
+
+let game = new Phaser.Game(config);
