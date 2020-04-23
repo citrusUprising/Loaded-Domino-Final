070f30b660b2a3e7571319178d0d2c040894e653
Camellia Shea <cashmill@ucsc.edu>
2020-04-23 13:56:17 -0700

    Implemented arcade physics

--- src/main.js
+++ src/main.js
@@ -11,7 +11,15 @@ let config = {
 	type: Phaser.CANVAS, /*Loads 2d web renderer*/
 	width: 960, /*game width (adjustable)*/
 	height: 540, /*game height (adjustable)*/
-	scene: [ Menu, Tutorial, Play ]
+	scene: [ Menu, Tutorial, Play ],
+	physics: {
+		default: "arcade",
+		arcade: {
+			gravity: {
+				y: 200
+			}
+		}
+    },
 };
 
 let game = new Phaser.Game(config);
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,15 +4,22 @@ class Play extends Phaser.Scene{
     }
     
     preload(){
-		//load assets
-		
+		// load temp images
+		this.load.image("player", "assets/player-temp.png");
     }
 
     create(){
-        //console.log(this);
+		this.player = new Player(this, 20, 20, "player", 0);
+		this.physics.add.existing(this.player);
+		
+		this.player.body.velocity.x = 100;
+		this.player.body.velocity.y = 100;
+		this.player.body.bounce.x = 1;
+		this.player.body.bounce.y = 1;
+		this.player.body.collideWorldBounds = true;
     }
 
     update(){
-
+		
     }
 }
