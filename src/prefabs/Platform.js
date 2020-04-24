8594bc28c84c430644ebb4dc0495a48491fcbfa2
senoel <snoelle@ucsc.edu>
2020-04-24 13:52:13 -0700

    added platform distruction and movement

--- src/prefabs/Platform.js
+++ src/prefabs/Platform.js
@@ -1,15 +1,19 @@
 class Platform extends Phaser.GameObjects.Sprite{
 	
-    constructor(scene, x, y, w, h, texture, frame){
+    constructor(scene, x, y, w, h, texture, frame, speed){
         super(scene, x, y, texture, frame);
 		this.x = x;
 		this.y = y;
 		this.width = w;
-		this.height = h;
+        this.height = h;
+        this.speed = 1;
     }
     
     update(){
-        
+
+        this.y -= this.speed; //moves platform up based on speed
+
+        if (this.y<0) delete this;//destroys platforms past the top of the screen, untested
     }
 
 }
