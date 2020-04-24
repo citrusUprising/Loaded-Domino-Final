fea5cb8aef21f391ae5b5be80201bced0170f3f3
senoel <snoelle@ucsc.edu>
2020-04-23 18:09:32 -0700

    tutorial exitable

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -8,10 +8,24 @@ class Tutorial extends Phaser.Scene{
     }
 
     create(){
+
+        this.comeplete = false;
+
         //console.log(this);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update(){
-        
+        this.comeplete = true;
+
+        if(this.comeplete){
+            if(Phaser.Input.Keyboard.JustDown(keyLEFT)){//left returns to menu
+                this.scene.start("menuScene");
+            }
+            if(Phaser.Input.Keyboard.JustDown(keyRIGHT)){//right starts game
+                this.scene.start("playScene");
+            }
+        }
     }
 }
