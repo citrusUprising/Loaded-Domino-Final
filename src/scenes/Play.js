5f7a102e93338c7e3e555a6f774b3e409fa1bb4f
citrusUprising <62976444+citrusUprising@users.noreply.github.com>
2020-04-26 12:57:25 -0700

    added timer, score, and ooze psuedocode

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -17,7 +17,15 @@ class Play extends Phaser.Scene {
         this.background = this.add.tileSprite(0, 0, 960, 540, "backDrop").setOrigin(0,0);
         //speed of scrolling
         this.scroll = 1;
-        this.platMod = -60;         
+        this.platMod = -60;   
+        this.score = 0;   
+        
+        this.speedTimer = this.time.addEvent ({
+            delay: 1000,
+            callback: this.scoreUP/*(this.score, this.speed)*/,
+            callbackScope: this,
+            loop: true
+        });
 
 		// group for platform collisions
         this.platforms = this.physics.add.group(); //shift to platform class??
@@ -35,7 +43,7 @@ class Play extends Phaser.Scene {
 
         // add timer for spawning platforms (possibly temporary method?)
         this.platformTimer = this.time.addEvent ({
-            delay: 3100-100*this.scroll,
+            delay: 3000, 
             callback: this.makePlatform,
             callbackScope: this,
             loop: true
@@ -58,6 +66,25 @@ class Play extends Phaser.Scene {
         //separate gameover variables depending on death
         this.gameoverTop = false;
         this.gameoverBot = false;
+
+        let menuConfig = {
+            fontFamily: 'Helvetica',
+            fontSize: '30px',
+            backgroundColor: '#000000',
+            color: '#facade',
+            align: 'center',
+            padding: {
+                top: 10,
+                bottom: 10,
+            },
+            fixedWidth: 100
+        }
+
+        let centerX = game.config.width/2;
+        let centerY = game.config.height/2;
+        let textSpacer = 80;
+
+        this.scoreBoard = this.add.text (0, 0, this.score, menuConfig);
 
         // assign keys
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
@@ -87,21 +114,22 @@ class Play extends Phaser.Scene {
 
             //flip sprite when player turns
             if (this.player.isTurn) { 
-                console.log('turning');
+                //console.log('turning');
                 this.player.toggleFlipX();
                 this.player.isTurn = false;
             }
             
             // check if player died
             if (this.player.y < -50) { //to be changed to collision with ooze //(this.player.body.overlapY>0)
-                console.log("game over, eaten by void"); //prints infinitely, adjust
+                console.log("game over, eaten by void");
                 this.gameoverTop=true;
-                //this.scene.restart();
+                //if(Oozeheight<game.config.height+50) this.Oozeheight ++;
             } else if (this.player.y > game.config.height+50) {
-                console.log("game over, fell of screen"); //prints infinitely, adjust
+                console.log("game over, fell of screen");
                 this.gameoverBot=true;
-                //this.scene.restart();
             }
+
+            this.scoreBoard.setText(this.score);
     
         // else, if player's dead
         } else {
@@ -146,8 +174,8 @@ class Play extends Phaser.Scene {
     // spawn platform randomly at bottom of screen
     makePlatform() {
 
-        let sx = Phaser.Math.RND.between(-50, game.config.width+50);
-        console.log(sx);
+        let sx = Phaser.Math.RND.between(-25, game.config.width+25);
+        //console.log(sx);
 
         let platform = this.platforms.create(sx, game.config.height+50, "platform");
 
@@ -158,5 +186,19 @@ class Play extends Phaser.Scene {
         platform.setFrictionX(1);
 
     }
+
+    scoreUP() {
+        if(!this.gameOverBot&&!this.gameOverTop){
+        this.score++;
+        console.log(this.score);
+        if(this.scroll<2&&this.score%20==0&&this.score>0){
+            this.scroll+= .2;
+            this.platformTimer.timeScale = 1+.1*this.scroll;
+            //update speed of existing platforms
+            /*Ooze Velocity Y = 0*/console.log('ooze Stop');
+        } else if (this.score%20==10/*&&OozeVariable<maxOozeHeight*/)/*Ooze Velocity Y = 1*/console.log('ooze Creep');
+        }
+    }
+
 
 }
