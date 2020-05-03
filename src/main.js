2783d1f712b464aecae01dd912794632716765d8
citrusUprising <62976444+citrusUprising@users.noreply.github.com>
2020-05-03 15:26:27 -0700

    added creative tilt

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 /*
  * WIP Name: Shelving Crisis
  * 
- * Single player infinite-climber
- * by Camellia Shea, Sean Noelle,  and Josepheng Xiong
+ * Single player infinite-dropper
+ * by Camellia Shea, Sean Noelle, and Josepheng Xiong
+ * Completed 5/3/2020
+ * 
+ * Visual Art by Josepheng Xiong
+ * Sound Effects by Camellia Shea
+ * To put a spin on endless runners, we wanted to make keeping up with the screen an actual part of the gameplay
+ * Unlike most endless runners, where the character essentially moves with the scrolling screen, ours forces player to constantly try to keep up
+ * Addiotionally, Threats are thrown at the player from both top and bottom instead of just one side.
+ * These two features together are an attempt to make players frantic and panic, as they not only have to deal with several different things at once,
+ * but are sometimes forced to deal with them by not doing anything (waiting for a platform).
  * 
  * */
 
